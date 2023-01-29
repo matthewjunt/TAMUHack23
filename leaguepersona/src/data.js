fa6461f7dffa888354beb2b9492e58315f7e1d05
Matthew Juntunen <matthewjunt@tamu.edu>
2023-01-28 19:07:04 -0600

    adding some data for personalities

---
+++ leaguepersona/src/data.js
@@ -0,0 +1,61 @@
+
+/* Aatrox - ISTJ
+    Ahri - ENFP
+    Akali - ISTP
+    Alistar - ISFJ
+    Amumu - ISFJ
+    Anivia - INTJ
+    Annie - ESFP
+    Ashe - ISFJ
+    Aurelion Sol - INTP
+    Azir - INTJ
+    Bard - ESFP
+    Blitzcrank - ISTJ
+    Brand - INTJ
+    Braum - ISFJ
+    Caitlyn - ISTJ
+    Camille - ISTJ
+    Cassiopeia - INTJ
+    Cho'Gath - ISTJ
+    Corki - ESTJ
+    Darius - ISTJ
+    Diana - ISTP
+    Dr. Mundo - ISTJ
+    Draven - ESTP
+    Ekko - INTP
+    Elise - ISTP
+    Evelynn - ENTP
+    Ezreal - ENTP
+    Fiddlesticks - INTJ
+    Fiora - ISTP
+    Fizz - ENTP
+    Galio - ISTJ
+    Gangplank - ESTP
+    Garen - ISTJ
+    Gnar - ISTP
+    Gragas - ISTJ
+    Graves - ESTJ
+    Hecarim - ISTJ
+    Heimerdinger - INTJ
+    Illaoi - ISTJ
+    Irelia - ISTP*/
+var characters = {"ISTJ" : ["Illaoi", ]}
+
+var questions
+
+var characters = {
+    ISTJ: ["Aatrox", "Alistar", "Amumu", "Braum", "Cho'Gath", "Darius", "Dr. Mundo", "Galio", "Gragas", "Hecarim", "Illaoi"],
+    ENFP: ["Ahri"],
+    ISTP: ["Akali", "Diana", "Elise", "Fiora", "Gnar", "Irelia"],
+    ISFJ: ["Alistar", "Amumu", "Ashe", "Braum"],
+    INTJ: ["Anivia", "Azir", "Brand", "Cassiopeia", "Fiddlesticks", "Heimerdinger"],
+    ESFP: ["Annie", "Bard"],
+    ISTJ: ["Blitzcrank", "Caitlyn", "Camille", "Corki", "Graves"],
+    INTP: ["Aurelion Sol", "Ekko"],
+    INTJ: ["Azir"],
+    ESFP: ["Bard"],
+    ISTJ: ["Blitzcrank", "Caitlyn", "Camille", "Corki", "Graves"],
+    ENTP: ["Evelynn", "Ezreal", "Fizz"],
+    ESTP: ["Draven", "Gangplank"],
+    ISTP: ["Diana", "Elise", "Fiora", "Gnar", "Irelia"],
+}
